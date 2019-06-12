37075edfbec3bad6bb731d8996c0fbf77bbf008c
Victor I. Afolabi <javafolabi@gmail.com>
2019-06-12 20:08:21 +0100

    Graph binary

--- src/connect.rs
+++ src/connect.rs
@@ -1,11 +1,17 @@
 #[macro_use]
 extern crate diesel;
 
-pub mod schema;
 pub mod models;
 
 use diesel::prelude::*;
 
+/// Establishes an SQLite connection.
+///
+/// # Example
+/// ```
+/// #[allow(unused_variable)]
+/// let conn: SqliteConnection = establish_connection("post.db");
+/// ```
 pub fn establish_connection(database_url: &str) -> SqliteConnection {
     SqliteConnection::establish(database_url)
     .expect(&format!("Error connecting to {}", database_url))
