96543aaa27df3ea050d7925d8efc6e5eb569a69a
Victor I. Afolabi <javafolabi@gmail.com>
2019-06-13 00:54:00 +0100

    Added more models to models.rs

--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,3 @@
-use std::collections::HashMap;
-
-
 /// Vertex (or Node) - representing each Entity in Graph.
 ///
 #[derive(Queryable)]
@@ -8,51 +5,88 @@ pub struct Vertex {
   /// Unique 8-bit token assigned to each Vertex.
   pub id: String,
 
-  /// Vertex label.
+  /// Label given to Vertex (or Node).
   pub label: String,
 
-  /// Vertex schema.
+  /// Vertex schema. Using https://schema.org standards.
+  /// Other forms of FOAF oncology will be supported in
+  /// future.
   pub schema: String,
 
-  /// Payload which current vertex carries. Contains information about Vertex.
-  pub payload: HashMap<String, String>,
+  /// Payload which current vertex carries.
+  /// Contains information about Vertex.
+  pub payload: Vec<Payload>,
 
   /// Connection of Vertex to other Vertex in the Graph.
   pub edges: Vec<Edge>,
 }
+/// Payload containing detailed information about Vertex.
+///
+/// **Note:** The payload doesn't hold a value of another Vertex
+/// object, otherwise, it contains data modeled by a certain
+/// Vertex.
+///
+/// E.g: "name": "John Doe", "age": 22
+///
+#[derive(Queryable)]
+pub struct Payload {
+  /// Table's primary key.
+  pub id: i32,
+  /// Key of payload. E.g "name", "age", etc...
+  pub key: String,
+  /// Value of payload. E.g. "John", 23, etc...
+  pub value: String,
+  /// Foreign key of Vertex which has this payload.
+  pub vertex_id: String,
+}
 
-
-/// Edge which is describes the connection between one Vertex & it's neighbors.
+/// Edge which is describes the connection between
+/// one Vertex & it's neighbors.
 ///
 #[derive(Queryable)]
 pub struct Edge {
 
   /// Table's primary key.
-  pub id: u32,
+  pub id: String,
 
-  /// Vertex which the edge is connected to.
-  pub vertex_id: String,
+  /// Destination vertex Identifier is the vertex identifier
+  ///  which the source vertex identifier is connected to.
+  pub dest_vertex_id: String,
 
-  /// Describing the connection `vertex` has with `vertex_id`.
+  /// Destination vertex is the vertex which the source
+  /// vertex is connected to.
+  pub dest_vertex: Vertex,
+
+  /// Describing the connection `src_vertex_id` has with
+  /// `dest_vertex_id`.
   pub predicate: String,
 
-  /// Source Vertex (`vertex`) is connected to `vertex_id`.
-  pub vertex: Vertex,
+  /// Source vertex identifier is the identifier of current
+  ///  vertex which is connected to `dest_vertex_id`.
+  pub src_vertex_id: String,
+
+  /// Source vertex is the current vertex  which is connected
+  /// to `dest_vertex_id`.
+  pub src_vertex: Vertex,
 }
 
 
-/// Graph database Schema.
+/// A collection of related information on a particular
+/// piece of linked data.
 ///
+/// **Note:** Sage KnowledgeGraph can contain multiple Graph.
 #[derive(Queryable)]
 pub struct Graph {
-  /// Unique integer primary key.
-  pub id: u32,
+  /// We can have multiple graphs in a Knowledge Graph.
+  pub id: String,
 
-  /// Graph name - DB Storage name.
+  /// A descriptive name given to the Graph. Name tells us
+  /// which type of information is contained in the Graph.
+  /// Example: "Medical Symptoms"
   pub name: String,
 
-  /// Vertex Id foreign key.
-  pub vertex_id: String,
+  /// A detailed description of the Graph content.
+  pub description: String,
 
   /// List of all Vertex objects in Graph.
   pub vertices: Vec<Vertex>,
