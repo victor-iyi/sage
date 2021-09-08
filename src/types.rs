db0b7300cc5636faecbe3c30fae0ae7994a84445
Victor I. Afolabi <javafolabi@gmail.com>
2021-09-07 19:18:37 -0500

    Update docs for

--- src/types.rs
+++ src/types.rs
@@ -105,18 +105,109 @@ impl fmt::Display for DType {
 }
 
 impl DType {
+  /// Index into a JSON array or map. A string index can be used to access a
+  /// value in a map, and a usize index can be used to access an element of an
+  /// array.
+  ///
+  /// # Examples
+  ///
+  /// ```rust
+  /// # use sage::json;
+  /// #
+  /// let object = json!({"A": 65, "B": 66, "C": 67});
+  /// assert_eq!(*object.get("A").unwrap(), json!(65));
+  ///
+  /// let array = json!(["A", "B", "C"]);
+  /// assert_eq!(*array.get(2).unwrap(), json!("C"));
+  ///
+  /// assert_eq!(array.get("A"), None);
+  /// ```
+  ///
+  /// Square brackets can also be used to index into a value in a more concise
+  /// way. This returns `DType::Null` in cases where `get` would have returned
+  /// `None`.
+  ///
+  /// ```rust
+  /// # use sage::json;
+  /// #
+  /// let object = json!({
+  ///     "A": ["a", "à", "â"],
+  ///     "B": ["b"],
+  ///     "C": ["C", "Ç", "Ć"]
+  /// });
+  /// assert_eq!(object["B"][0], json!("b"));
+  ///
+  /// assert_eq!(object["D"], json!(null));
+  /// assert_eq!(object[0]["x"]["y"]["z"], json!(null));
+  /// ```
+  ///
   pub fn get<I: index::Index>(&self, index: I) -> Option<&DType> {
     index.index_into(self)
   }
 
+  /// Mutably index into a JSON array or map. A string index can be used to
+  /// access a value in a map, and a usize index can be used to access an
+  /// element of an array.
+  ///
+  /// Returns `None` if the type of `self` does not match the type of the
+  /// index, for example if the index is a string and `self` is an array or a
+  /// number. Also returns `None` if the given key does not exist in the map
+  /// or the given index is not within the bounds of the array.
+  ///
+  /// ```rust
+  /// # use sage::json;
+  /// #
+  /// let mut object = json!({ "A": 65, "B": 66, "C": 67});
+  /// *object.get_mut("A").unwrap() = json!(69);
+  ///
+  /// let mut array = json!(["A", "B", "C"]);
+  /// *array.get_mut(2).unwrap() = json!("D");
+  /// ```
   pub fn get_mut<I: index::Index>(&mut self, index: I) -> Option<&mut DType> {
     index.index_into_mut(self)
   }
 
+  /// Returns true if the `DType` is an Object. Returns false otherwise.
+  ///
+  /// For any value on which `is_object` returns true, `as_object` and
+  /// `as_object_mut` are guarnteed to return the map representation of the
+  /// object.
+  ///
+  /// ```rust
+  /// # use sage::json;
+  /// #
+  /// let obj = json!({
+  ///   "a": { "nested": true },
+  ///   "b": ["an", "array"]
+  /// });
+  ///
+  /// assert!(obj.is_object());
+  /// assert!(obj["a"].is_object());
+  ///
+  /// // array, not an object.
+  /// assert!(!obj["b"].is_object());
+  /// ```
   pub fn is_object(&self) -> bool {
     self.as_object().is_some()
   }
 
+  /// If the `DType` is an Object, returns the associated `Map`. Returns `None`
+  /// otherwise.
+  ///
+  /// ```rust
+  /// # use sage::json;
+  /// #
+  /// let obj = json!({
+  ///   "a": { "nested": true },
+  ///   "b": ["an", "array"]
+  /// });
+  ///
+  /// // The length of `{"nested": true} is 1 entry.
+  /// assert_eq!(obj["a"].as_object().unwrap().len(), 1);
+  ///
+  /// // The array `["an", "array"] is not an object.
+  /// assert_eq!(obj["b"].as_object(), None);
+  /// ```
   pub fn as_object(&self) -> Option<&Map<String, DType>> {
     match *self {
       DType::Object(ref m) => Some(m),
@@ -124,6 +215,17 @@ impl DType {
     }
   }
 
+  /// If the `DType` is an Object, returns the associated mutable `Map` (`&mut Map`).
+  /// Returns `None` otherwise.
+  ///
+  /// ```rust
+  /// # use sage::json;
+  /// #
+  /// let mut obj = json!({"a": { "nested": true }});
+  ///
+  /// obj["a"].as_object_mut().unwrap().clear();
+  /// assert_eq!(obj, json!({ "a": {} }));
+  /// ```
   pub fn as_object_mut(&mut self) -> Option<&mut Map<String, DType>> {
     match *self {
       DType::Object(ref mut m) => Some(m),
@@ -131,10 +233,45 @@ impl DType {
     }
   }
 
+  /// Returns true if the `DType` is an Array. Returns false otherwise.
+  ///
+  /// For any `DType` on which `is_array` returns true, `as_array` and
+  /// `as_array_mut` are guaranteed to return the vector representing the
+  /// array.
+  ///
+  /// ```rust
+  /// # use sage::json;
+  /// #
+  /// let obj = json!({
+  ///   "a": ["an", "array"],
+  ///   "b": { "an": "object" }
+  /// });
+  /// assert!(obj["a"].is_array());
+  ///
+  /// // an object, not an array.
+  /// assert!(!obj["b"].is_array());
+  /// ```
   pub fn is_array(&self) -> bool {
     self.as_array().is_some()
   }
 
+  /// If the `DType` is an Array, returns the associated vector. Returns `None`
+  /// otherwise.
+  ///
+  /// ```rust
+  /// # use sage::json;
+  /// #
+  /// let obj = json!({
+  ///   "a": ["an", "array"],
+  ///   "b": {"an": "object"},
+  /// });
+  ///
+  /// // The length of `["an", "array"]` is 2 elements.
+  /// assert_eq!(obj["a"].as_array().unwrap().len(), 2);
+  ///
+  /// // The object `{"an": "object"}` is not an array.
+  /// assert_eq!(obj["b"].as_array(), None);
+  /// ```
   pub fn as_array(&self) -> Option<&Vec<DType>> {
     match *self {
       DType::Array(ref a) => Some(&*a),
@@ -142,6 +279,17 @@ impl DType {
     }
   }
 
+  /// If the `DType` is an Array, returns the associated mutable vector (`&mut Vec<DType>`).
+  /// Returns `None` otherwise.
+  ///
+  /// ```rust
+  /// # use sage::json;
+  /// #
+  /// let mut obj = json!({ "a": ["an", "array"] });
+  ///
+  /// obj["a"].as_array_mut().unwrap().clear();
+  /// assert_eq!(obj, json!({ "a": [] }));
+  /// ```
   pub fn as_array_mut(&mut self) -> Option<&mut Vec<DType>> {
     match *self {
       DType::Array(ref mut v) => Some(v),
@@ -149,20 +297,45 @@ impl DType {
     }
   }
 
+  /// Returns true if the `DType` is a String. Returns false otherwise.
+  ///
+  /// For any `DType` on which `is_string` returns true, `as_str` is guaranteed to
+  /// return the string slice.
+  ///
+  /// ```rust
+  /// # use sage::json;
+  /// #
+  /// let obj = json!({ "a": "some string", "b": false });
+  /// assert!(obj["a"].is_string());
+  ///
+  /// // The boolean `false` is not a string
+  /// assert!(!obj["b"].is_string());
+  /// ```
   pub fn is_string(&self) -> bool {
     self.as_str().is_some()
   }
+
   /// If the `DType` is a String, returns the associated str. Returns None
   /// otherwise
   ///
   /// ```rust
-  /// # use sage::DType;
+  /// # use sage::json;
   /// #
-  /// let foo = DType::String("foo".to_string());
-  /// assert_eq!(foo.as_str(), Some("foo"));
+  /// let obj = json!({ "a": "some string", "b": false });
+  /// assert!(obj["a"].is_string());
   ///
-  /// // The value is: "foo"
-  /// println!("The value of foo: {}", foo);
+  /// // The boolean `false` is not a string
+  /// assert!(!obj["b"].is_string());
+  ///
+  /// // JSON values are printed in JSON representation, so strings are in quotes.
+  /// //
+  /// // The value is "some string"
+  /// println!("The value is: {}", obj["a"]);
+  ///
+  /// // Rust strings are printed without quotes.
+  /// //
+  /// //  The value is: some string
+  /// println!("The value is: {}", obj["a"].as_str().unwrap());
   /// ```
   pub fn as_str(&self) -> Option<&str> {
     match *self {
@@ -173,6 +346,15 @@ impl DType {
 
   /// Returns true if the `DType` is a number. Returns false otherwise.
   ///
+  /// ```rust
+  /// # use sage::json;
+  /// #
+  /// let obj = json!({ "a": 1, "b": "2" });
+  /// assert!(obj["a"].is_number());
+  ///
+  /// // The string "2" is a string, not a number.
+  /// assert!(!obj["b"].is_number());
+  /// ```
   pub fn is_number(&self) -> bool {
     matches!(*self, DType::Number(_))
   }
@@ -183,6 +365,19 @@ impl DType {
   /// For any `DType` on which `is_i64` returns true, `as_i64` is guaranteed to
   /// return the integer value.
   ///
+  /// ```rust
+  /// # use sage::json;
+  /// #
+  /// let big = i64::MAX as u64 + 10;
+  /// let obj = json!({ "a": 65, "b": big, "c": 256.0 });
+  /// assert!(obj["a"].is_i64());
+  ///
+  /// // Greater than i64::MAX
+  /// assert!(!obj["b"].is_i64());
+  ///
+  /// // Numbers with a decimal point are not considered integers.
+  /// assert!(!obj["c"].is_i64());
+  /// ```
   pub fn is_i64(&self) -> bool {
     matches!(*self, DType::Number(ref n) if n.is_i64())
   }
@@ -192,6 +387,18 @@ impl DType {
   /// For any `DType` on which `is_u64` returns true, `as_u64` is guaranteed to
   /// return the integer value.
   ///
+  /// ```rust
+  /// # use sage::json;
+  /// #
+  /// let obj = json!({ "a": 65, "b": -65, "c": 256.0 });
+  /// assert!(obj["a"].is_u64());
+  ///
+  /// // Negative ingter.
+  /// assert!(!obj["b"].is_u64());
+  ///
+  /// // Numbers with a decimal point are not considered integers.
+  /// assert!(!obj["c"].is_u64());
+  /// ```
   pub fn is_u64(&self) -> bool {
     matches!(*self, DType::Number(ref n) if n.is_u64())
   }
@@ -204,17 +411,33 @@ impl DType {
   /// Currently this function returns true if and only if both `is_i64` and
   /// `is_u64` return false but this is not a guarantee in the future.
   ///
+  /// ```rust
+  /// # use sage::json;
+  /// #
+  /// let obj = json!({ "a": 256.0, "b": 65, "c": -65 });
+  /// assert!(obj["a"].is_f64());
+  ///
+  /// // Integers.
+  /// assert!(!obj["b"].is_f64());
+  /// assert!(!obj["c"].is_f64());
+  /// ```
   pub fn is_f64(&self) -> bool {
-    // match *self {
-    //   DType::Number(ref n) => n.is_f64(),
-    //   _ => false,
-    // }
     matches!(*self, DType::Number(ref n) if n.is_f64())
   }
 
   /// If the `DType` is an integer, represent it as `i64` if possible. Returns
   /// `None` otherwise.
   ///
+  /// ```rust
+  /// # use sage::json;
+  /// #
+  /// let big = i64::MAX as u64 + 10;
+  /// let obj = json!({ "a": 65, "b": big, "c": 256.0 });
+  ///
+  /// assert_eq!(obj["a"].as_i64(), Some(65));
+  /// assert_eq!(obj["b"].as_i64(), None);
+  /// assert_eq!(obj["c"].as_i64(), None);
+  /// ```
   pub fn as_i64(&self) -> Option<i64> {
     match *self {
       DType::Number(ref n) => n.as_i64(),
@@ -225,6 +448,15 @@ impl DType {
   /// If the `DType` is an integer, represent it as `u64` if possible. Returns
   /// `None` otherwise.
   ///
+  /// ```rust
+  /// # use sage::json;
+  /// #
+  /// let obj = json!({ "a": 65, "b": -65, "c": 256.0 });
+  ///
+  /// assert_eq!(obj["a"].as_u64(), Some(65));
+  /// assert_eq!(obj["b"].as_u64(), None);
+  /// assert_eq!(obj["c"].as_u64(), None);
+  /// ```
   pub fn as_u64(&self) -> Option<u64> {
     match *self {
       DType::Number(ref n) => n.as_u64(),
@@ -235,6 +467,15 @@ impl DType {
   /// If the `DType` is a number, represent it as `f64` if possible. Returns
   /// `None` otherwise.
   ///
+  /// ```rust
+  /// # use sage::json;
+  /// #
+  /// let obj = json!({ "a": 256.0, "b": 65, "c": -65 });
+  ///
+  /// assert_eq!(obj["a"].as_f64(), Some(256.0));
+  /// assert_eq!(obj["b"].as_f64(), Some(65.0));
+  /// assert_eq!(obj["c"].as_f64(), Some(-65.0));
+  /// ```
   pub fn as_f64(&self) -> Option<f64> {
     match *self {
       DType::Number(ref n) => n.as_f64(),
@@ -248,14 +489,13 @@ impl DType {
   /// guaranteed to return the boolean value.
   ///
   /// ```rust
-  /// # use sage::DType;
+  /// # use sage::json;
   /// #
-  /// let value = DType::Boolean(false);
-  /// assert!(value.is_bool());
+  /// let obj = json!({ "a": false, "b": "false" });
+  /// assert!(obj["a"].is_bool());
   ///
   /// // The string "false" is a string, not a boolean.
-  /// let not_bool = DType::String("false".to_string());
-  /// assert!(!not_bool.is_bool());
+  /// assert!(!obj["b"].is_bool());
   /// ```
   pub fn is_bool(&self) -> bool {
     self.as_bool().is_some()
@@ -265,14 +505,13 @@ impl DType {
   /// Returns `None` otherwise.
   ///
   /// ```rust
-  /// # use sage::DType;
+  /// # use sage::json;
   /// #
-  /// let value = DType::Boolean(false);
-  /// assert_eq!(value.as_bool(), Some(false));
+  /// let obj = json!({ "a": false, "b": "false" });
+  /// assert_eq!(obj["a"].as_bool(), Some(false));
   ///
   /// // The string "false" is a string, not a boolean.
-  /// let not_bool = DType::String("false".to_string());
-  /// assert_eq!(not_bool.as_bool(), None)
+  /// assert_eq!(obj["b"].as_bool(), None);
   /// ```
   pub fn as_bool(&self) -> Option<bool> {
     match *self {
@@ -287,14 +526,13 @@ impl DType {
   /// to return `Some(())`.
   ///
   /// ```rust
-  /// # use sage::DType;
+  /// # use sage::json;
   /// #
-  /// let value = DType::Null;
-  /// assert!(value.is_null());
+  /// let obj = json!({ "a": null, "b": false });
+  /// assert!(obj["a"].is_null());
   ///
   /// // The boolean `false` is not null.
-  /// let not_null =  DType::Boolean(false);
-  /// assert!(!not_null.is_null());
+  /// assert!(!obj["b"].is_null());
   /// ```
   pub fn is_null(&self) -> bool {
     self.as_null().is_some()
@@ -303,14 +541,13 @@ impl DType {
   /// If the `DType` is a `Null`, return `()` *(unit type)*. Returns `None` otherwise.
   ///
   /// ```
-  /// # use sage::DType;
+  /// # use sage::json;
   /// #
-  /// let value = DType::Null;
-  /// assert_eq!(value.as_null(), Some(()));
+  /// let obj = json!({ "a": null, "b": false });
+  /// assert_eq!(obj["a"].as_null(), Some(()));
   ///
   /// // The boolean `false` is not null.
-  /// let not_null = DType::Boolean(false);
-  /// assert_eq!(not_null.as_null(), None);
+  /// assert_eq!(obj.as_null(), None);
   /// ```
   pub fn as_null(&self) -> Option<()> {
     match *self {
@@ -324,18 +561,48 @@ impl DType {
   /// # Example
   ///
   /// ```rust
-  /// # use sage::DType;
+  /// # use sage::json;
   ///
-  /// let mut value = DType::String("Foo".to_string());
-  /// assert_eq!(value.take(), DType::String("Foo".to_string()));
+  /// let mut obj = json!({ "x": "y" });
+  /// assert_eq!(obj["x"].take(), json!("y"));
   ///
-  /// assert_eq!(value, DType::Null);
+  /// assert_eq!(obj, json!({ "x": null }));
   /// ```
   pub fn take(&mut self) -> DType {
     std::mem::replace(self, DType::Null)
   }
 }
 
+/// The default value is `DType::Null`.
+///
+/// This is useful for handling omitted `DType` fields when deserializing.
+///
+/// # Examples
+///
+/// ```rust,ignore
+/// # use serde::Deserialize;
+/// # use sage::Result;
+/// use sage::json;
+///
+/// #[derive(Deserialize)]
+/// struct Settings {
+///     level: i32,
+///     #[serde(default)]
+///     extras: DType,
+/// }
+///
+/// # fn try_main() -> Result<()> {
+/// let data = r#" { "level": 42 } "#;
+/// let s: Settings = sage::from_str(data)?;
+///
+/// assert_eq!(s.level, 42);
+/// assert_eq!(s.extras, DType::Null);
+/// #
+/// #     Ok(())
+/// # }
+/// #
+/// # try_main().unwrap()
+/// ```
 impl Default for DType {
   fn default() -> DType {
     DType::Null
